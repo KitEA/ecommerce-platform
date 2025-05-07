a8eeed1b2f59b3a7c0d3e3b0d7eff6d7611775b8
KitEA <kit.earf@gmail.com>
2025-05-07 15:48:55 +0400

    refactor ProductController to use @ModelAttribute

--- src/main/java/com/kit/ecommerce_platform/controller/ProductController.java
+++ src/main/java/com/kit/ecommerce_platform/controller/ProductController.java
@@ -4,10 +4,7 @@ import com.kit.ecommerce_platform.dto.ProductSearchCriteria;
 import com.kit.ecommerce_platform.model.Product;
 import com.kit.ecommerce_platform.service.ProductSearchService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,9 +16,7 @@ public class ProductController {
     private final ProductSearchService searchService;
 
     @GetMapping("/search")
-    public ResponseEntity<List<Product>> searchProducts(ProductSearchCriteria criteria) {
-        return ResponseEntity.ok(
-                searchService.searchProducts(criteria)
-        );
+    public List<Product> searchProducts(@ModelAttribute ProductSearchCriteria criteria) {
+        return searchService.searchProducts(criteria);
     }
 }
